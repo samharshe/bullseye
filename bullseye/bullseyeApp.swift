b31bd26c420113adcea77cc54c2cf30b54fb15e4
samharshe <samharshe29@gmail.com>
2021-05-13 19:54:09 -0400

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Sam Harshe on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
